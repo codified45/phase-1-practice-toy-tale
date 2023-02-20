d135b0abed0823462ba8900d3b5b3b9ae68e6ed2
Jack Lach <jack.lach4@gmail.com>
2023-02-20 05:50:13 -0500

    get likes to update in DOM and persist in db

--- src/index.js
+++ src/index.js
@@ -20,10 +20,8 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(res => res.json())
       .then(array => {
         for (const object in array) {
-        console.log(array[object]);
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card');
-        console.log(cardDiv);
         const h2 = document.createElement('h2');
         h2.textContent = array[object].name;
         const img = document.createElement('img');
@@ -32,17 +30,103 @@ document.addEventListener("DOMContentLoaded", () => {
         const p = document.createElement('p');
         p.textContent = `${array[object].likes} Likes`;
         const btn = document.createElement('button');
-        console.log(array[object].id);
         btn.id = array[object].id;
-        console.log(btn.id);
         btn.classList.add('like-btn');
-        btn.textContent = "Like ❤️"
+        btn.textContent = "Like ❤️";
+        btn.addEventListener('click', likeButtonClickHandler);
         cardDiv.append(h2, img, p, btn);
         cardContainer.appendChild(cardDiv);
       }});
+  
+  let toyForm = document.querySelector('.container .add-toy-form');
+  toyForm.addEventListener('submit', handleToySubmitData);
+
+  const submitData = (toyNameString, toyImageUrlString) => {
+    let formData = {
+      name: toyNameString,
+      image: toyImageUrlString,
+      likes: 0,
+    };
+
+    const configPost = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+
+    fetch("http://localhost:3000/toys", configPost)
+      .then(res => res.json())
+        .then(object => {
+          const cardDiv = document.createElement('div');
+          cardDiv.classList.add('card');
+          const h2 = document.createElement('h2');
+          h2.textContent = object.name;
+          const img = document.createElement('img');
+          img.src = object.image;
+          img.classList.add('toy-avatar');
+          const p = document.createElement('p');
+          p.textContent = '0 Likes';
+          const btn = document.createElement('button');
+          btn.id = object.id;
+          btn.classList.add('like-btn');
+          btn.textContent = "Like ❤️";
+          btn.addEventListener('click', likeButtonClickHandler);
+          cardDiv.append(h2, img, p, btn);
+          cardContainer.appendChild(cardDiv);
+        })
+  };
+
+
+  function handleToySubmitData(e){
+    e.preventDefault();
+    let toyNameString = toyForm.name.value;
+    let toyImageUrlString = toyForm.image.value;
+    submitData(toyNameString, toyImageUrlString);
+    toyForm.reset();
+  };
+
+  function likeButtonClickHandler(e){
+    console.log(`I've been clicked by Button ${e.target.id}`);
+    let btnId = e.target.id;
+    fetchUrl = `http://localhost:3000/toys/${btnId}`;
+    // let likes = e.paerentNode
+    console.log(e);
+    console.log(e.target.parentNode);
+    let card = e.target.parentNode;
+    let likeText = card.querySelector('p').textContent;
+    let likeNumber = parseInt(likeText);
+    console.log(likeText);
+    console.log(likeNumber);
+    let newLikeNumber = likeNumber + 1;
+    console.log(newLikeNumber);
+    likeText = `${newLikeNumber} Likes`;
+    console.log(likeText);
+    card.querySelector('p').textContent = likeText;
+    console.log(card.querySelector('p').textContent);
+
+    let newLikeData = {
+      likes: newLikeNumber,
+    };
+
+    const configPatch = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(newLikeData),
+    };
+
+    fetch(fetchUrl, configPatch)
+      .then(res => res.json())
+        .then(object => {
+          console.log(object)
+          
+        })
 
   
-
-
-  // cardDiv.appendChild('foo');
+  };
 });
